a78002ef6fd44214aa92fd4661495fece5dfbdc4
Thomasorus <contact@thomasorus.com>
2022-12-29 23:36:55 +0100

    Scrapper seems to scrap

--- scraper.py
+++ scraper.py
@@ -5,7 +5,35 @@ import requests
 import shutil
 from bs4 import BeautifulSoup
 from slugify import slugify
+import subprocess
 
+
+
+
+# listing_pages = [
+#     "https://wiki.basgrospoing.fr/index.php/Sp%C3%A9cial:Toutes_les_pages?from=&to=&namespace=0&hideredirects=1",
+#     "https://wiki.basgrospoing.fr/index.php?title=Sp%C3%A9cial:Toutes_les_pages&from=Melty+Blood%2FAkiha+Tohno+%28Seifuku%29%2FFull+Moon&hideredirects=1",
+#     "https://wiki.basgrospoing.fr/index.php?title=Sp%C3%A9cial:Toutes_les_pages&from=Trish+%28UMVC3%29&hideredirects=1"
+# ]
+
+# for page in listing_pages:
+#     page = requests.get(page)
+#     soup = BeautifulSoup(page.content, "html.parser")
+#     links = soup.find("ul", class_='mw-allpages-chunk').find_all("a")
+#     for link in links:
+#         entry = {"text": link.text, "src": link['href']}
+#         print(entry)
+# Copy from terminal and clean the json
+
+
+def create_folder(folder):
+    if os.path.exists(folder):
+        print("The folder for " + folder + " already exists.")
+    else:
+        print("Creating " + folder + " folder.")
+        os.makedirs(folder)
+
+url = "https://wiki.basgrospoing.fr/index.php/"
 cache_path = "cache.json"
 cache_file = open(cache_path, "r")
 cache_text = cache_file.read()
@@ -13,16 +41,11 @@ cache_content = json.loads(cache_text)
 cache_file.close()
 
 for page in cache_content:
-    print(page)
-    res = requests.get(page)
-    soup = BeautifulSoup(res.content, "html.parser")
-    title = soup.find("h1")
-    title = title.contents[0]
-    print(title)
-    main = str(soup.find("main"))
-    re.sub(r"<p><br/>[\S\s]<\/p>", "", main)
-    re.sub(r"<\/ul>(.+?)<ul>", "", main)
-    main = main.replace("<br/>", "")
-    file = open(slugify(title) + '.html', 'w')
-    file.write(main)
-    file.close()
+    src = page["src"].split("/")
+
+    if len(src) > 1:
+        create_folder("site/" + src[0])
+
+    subprocess.run("monolith " + url + page['src'] + " -o site/" + page['src'] + ".html", shell=True)
+
+
